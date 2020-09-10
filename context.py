32f88dd9fda86eee154ec21d74a75a6c67a12de0
absop <zlang0@163.com>
2020-09-10 13:16:07 +0800

    fix some bugs.

--- context.py
+++ context.py
@@ -7,7 +7,7 @@ import sublime
 import sublime_plugin
 
 
-class DeleteToCommand(sublime_plugin.TextCommand):
+class ContextDeleteToCommand(sublime_plugin.TextCommand):
     def run(self, edit, to="eof"):
         region, selected = None, self.view.sel()[0]
         if to == "eof":
@@ -17,7 +17,7 @@ class DeleteToCommand(sublime_plugin.TextCommand):
         self.view.erase(edit, region)
 
 
-class FindToCommand(sublime_plugin.TextCommand):
+class ContextFindToCommand(sublime_plugin.TextCommand):
     def run(self, edit, to="eof"):
         selected = self.view.sel()[0]
         pattern = self.view.substr(selected)
@@ -29,6 +29,69 @@ class FindToCommand(sublime_plugin.TextCommand):
 
         regions = filter(compare, self.view.find_all(pattern))
         self.view.sel().add_all(regions)
+
+
+class InitialInputHandler(sublime_plugin.TextInputHandler):
+    def placeholder(self):
+        return "First Number"
+
+    def initial_text(self):
+        return "1"
+
+    def validate(self, numstr):
+        try:
+            int(numstr)
+            return True
+        except:
+            return False
+
+
+class ContextNumberSelectionsCommand(sublime_plugin.TextCommand):
+    def run(self, edit, initial):
+        sels = self.view.sel()
+        lmn = len(str(len(sels)))
+        for i in range(len(sels) - 1, -1, -1):
+            numstr = "{:0>{}}".format(i + int(initial), lmn)
+            self.view.insert(edit, sels[i].end(), numstr)
+
+    def input(self, args):
+        return InitialInputHandler()
+
+
+class ContextRemoveCommentCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        inline_comments = self.view.find_by_selector("comment.line")
+        inline_comments.reverse()
+        for comment in inline_comments:
+            if self.view.classify(comment.a) & sublime.CLASS_LINE_START:
+                self.view.erase(edit, comment)
+            else:
+                full_line = self.view.full_line(comment.a)
+                region = sublime.Region(full_line.a, comment.a)
+                if self.view.substr(region).isspace():
+                    self.view.erase(edit, full_line)
+                else:
+                    comment.b = comment.b - 1
+                    self.view.erase(edit, comment)
+
+        block_comments = self.view.find_by_selector("comment.block")
+        block_comments.reverse()
+        for comment in block_comments:
+            if self.view.classify(comment.a) & sublime.CLASS_LINE_START:
+                self.view.erase(edit, comment)
+            elif self.view.classify(comment.a) & sublime.CLASS_LINE_END:
+                self.view.replace(edit, comment, "\n")
+            else:
+                full_lines = self.view.full_line(comment)
+                region = sublime.Region(full_lines.a, comment.a)
+                if self.view.substr(region).isspace():
+                    comment.a = full_lines.a
+                self.view.erase(edit, comment)
+
+        trailing_white_space = self.view.find_all("[\t ]+$")
+        trailing_white_space.reverse()
+        for r in trailing_white_space:
+            self.view.erase(edit, r)
 
 
 class SystemOpenWith(sublime_plugin.TextCommand):
@@ -70,66 +133,3 @@ class SystemOpenWithDefaultApplicationCommand(SystemOpenWith):
         #     subprocess.call(["xdg-open", self.view.file_name()])
         # elif sublime.platform() == "osx":
         #     subprocess.call(["open", self.view.file_name()])
-
-
-class InitialInputHandler(sublime_plugin.TextInputHandler):
-    def placeholder(self):
-        return "First Number"
-
-    def initial_text(self):
-        return "1"
-
-    def validate(self, numstr):
-        try:
-            int(numstr)
-            return True
-        except:
-            return False
-
-
-class NumberSelectionsCommand(sublime_plugin.TextCommand):
-    def run(self, edit, initial):
-        sels = self.view.sel()
-        lmn = len(str(len(sels)))
-        for i in range(len(sels) - 1, -1, -1):
-            numstr = "{:0>{}}".format(i + int(initial), lmn)
-            self.view.insert(edit, sels[i].end(), numstr)
-
-    def input(self, args):
-        return InitialInputHandler()
-
-
-class RemoveCommentCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        inline_comments = self.view.find_by_selector("comment.line")
-        inline_comments.reverse()
-        for comment in inline_comments:
-            if self.view.classify(comment.a) & sublime.CLASS_LINE_START:
-                self.view.erase(edit, comment)
-            else:
-                full_line = self.view.full_line(comment.a)
-                region = sublime.Region(full_line.a, comment.a)
-                if self.view.substr(region).isspace():
-                    self.view.erase(edit, full_line)
-                else:
-                    comment.b = comment.b - 1
-                    self.view.erase(edit, comment)
-
-        block_comments = self.view.find_by_selector("comment.block")
-        block_comments.reverse()
-        for comment in block_comments:
-            if self.view.classify(comment.a) & sublime.CLASS_LINE_START:
-                self.view.erase(edit, comment)
-            elif self.view.classify(comment.a) & sublime.CLASS_LINE_END:
-                self.view.replace(edit, comment, "\n")
-            else:
-                full_lines = self.view.full_line(comment)
-                region = sublime.Region(full_lines.a, comment.a)
-                if self.view.substr(region).isspace():
-                    comment.a = full_lines.a
-                self.view.erase(edit, comment)
-
-        trailing_white_space = self.view.find_all("[\t ]+$")
-        trailing_white_space.reverse()
-        for r in trailing_white_space:
-            self.view.erase(edit, r)
