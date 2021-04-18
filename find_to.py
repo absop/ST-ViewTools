f091be2361fce4a60697465a972ebb3024f7dc2e
absop <zlang0@163.com>
2021-04-19 01:58:25 +0800

    Improved find_to command

--- find_to.py
+++ find_to.py
@@ -3,6 +3,8 @@ import sublime_plugin
 
 class FindToCommand(sublime_plugin.TextCommand):
     def run(self, edit, to='eof'):
+        if not self.view.has_non_empty_selection_region():
+            self.view.run_command('find_under_expand')
         selected = self.view.sel()[0]
         pattern = self.view.substr(selected)
 
