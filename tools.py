763bb8d73debd4b3e2ce9cce0dbfcca5f0edcab2
叶芝秋 <zlang0@163.com>
2022-01-02 02:46:48 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,15 @@
+import sublime
 import sublime_plugin
+
+
+class DeleteToCommand(sublime_plugin.TextCommand):
+    def run(self, edit, to='eof'):
+        region, selected = None, self.view.sel()[0]
+        if to == 'eof':
+            region = sublime.Region(selected.end(), self.view.size())
+        elif to == 'bof':
+            region = sublime.Region(0, selected.begin())
+        self.view.erase(edit, region)
 
 
 class FindToCommand(sublime_plugin.TextCommand):
